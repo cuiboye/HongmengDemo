c62ce30df0344dbc8a067032e9d05a7b9fab4257
dk_cuiboye@dakai.onaliyun.com <cuiboye@wajiu.com>
2024-05-05 10:38:01 +0800

    沉浸式屏幕+mvvm

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -8,7 +8,7 @@ export default class EntryAbility extends UIAbility {
   async onCreate(want, launchParam) {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
 
-    // 加载Preferences
+    // 初始化Preferences
     await PreferencesUtil.loadPreference(this.context, 'MyPreferences')
   }
 
@@ -20,7 +20,8 @@ export default class EntryAbility extends UIAbility {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
 
-    windowStage.loadContent('pages/Index', (err, data) => {
+
+    windowStage.loadContent('pages/Splash', (err, data) => {
       if (err.code) {
         hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
         return;
@@ -30,6 +31,50 @@ export default class EntryAbility extends UIAbility {
     StatusBarManager.get().storeWindowStage(windowStage);
 
     // StatusBarManager.get().setImmersiveStatusBar(windowStage);
+
+
+    let windowClass: window.Window = null;
+    windowStage.getMainWindow((err, data) => {
+      // 1.获取应用主窗口
+      windowClass = data;
+
+      // 2.设置底部navigation导航栏显示、状态栏不显示。注意：需要显示哪个数组中就写哪个就行
+      //status：顶部状态栏
+      //navigation：底部导航栏
+      //下面的数组中要设置“navigation”，如果不设置在有虚拟按键的情况下会不显示底部的导航
+      windowClass.setWindowSystemBarEnable(['status','navigation'], (err) => {
+
+      });
+      // 3.设置全屏
+      windowClass.setWindowLayoutFullScreen(true).then(() => {
+
+      })
+
+      windowClass.setWindowSystemBarProperties({
+        statusBarColor: '#00a0e9',
+        navigationBarColor: '#00a0e9',
+        // 以下两个属性从API Version 8开始支持A03d00/JSAPP
+        statusBarContentColor: '#00a0e9',
+        navigationBarContentColor: '#00a0e9'
+      })
+
+      // 4.获取底部导航栏高度，此时的高度单位是px
+      let navigationHeight = windowClass.getWindowAvoidArea(window.AvoidAreaType.TYPE_SYSTEM).bottomRect.height
+      let topStatusHeight = windowClass.getWindowAvoidArea(window.AvoidAreaType.TYPE_SYSTEM).topRect.height
+      AppStorage.SetOrCreate<number>('topStatusHeight', topStatusHeight)
+      AppStorage.SetOrCreate<number>('navigationHeight', navigationHeight)
+
+      // // TYPE_CUTOUT表示刘海屏区域。
+      // let cutOutInfo=windowClass.getWindowAvoidArea(window.AvoidAreaType.TYPE_CUTOUT);
+      // console.log(JSON.stringify(cutOutInfo));
+      // // TYPE_SYSTEM 表示系统默认区域。一般包括状态栏、导航栏，各设备系统定义可能不同。
+      // let navigationHeight = windowClass.getWindowAvoidArea(window.AvoidAreaType.TYPE_SYSTEM).bottomRect.height;
+      // AppStorage.SetOrCreate<number>('navigationHeight', navigationHeight)
+      // let topStatusHeight = windowClass.getWindowAvoidArea(window.AvoidAreaType.TYPE_SYSTEM).topRect.height;
+      // AppStorage.SetOrCreate<number>('topStatusHeight', topStatusHeight)
+
+
+    })
   }
 
   onWindowStageDestroy() {
